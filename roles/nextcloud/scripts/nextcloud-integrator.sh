5c63f1594d65fecad05c25c854ae6afecc4fe917
Giorgio Rengucci <37120983+GiorgioRen@users.noreply.github.com>
2023-02-14 12:52:25 +0100

    breaking changes

--- roles/nextcloud/scripts/nextcloud-integrator.sh
+++ roles/nextcloud/scripts/nextcloud-integrator.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 #
-# Install jq
-apt update && apt install jq -y
 # Helper functions
 #
 # Nextcloud
@@ -31,6 +29,8 @@ until curl -sSf http://keycloak:8080; do
 done
 echo 'Keycloak alive'
 
+sudo -u www-data /entrypoint.sh apache2-foreground &
+
 OIDC_CLIENT_SECRET=$(keycloakCurl http://keycloak:8080/admin/realms/vcc/clients/nextcloud | jq -r '.secret')
 
 # Wait until Nextcloud install is complete
@@ -48,13 +48,15 @@ setBoolean allow_user_to_change_display_name false
 setString lost_password_link disabled
 setBoolean oidc_login_disable_registration false
 
-setString oidc_login_provider_url "http://keycloak:8080/realms/vcc"
+setString oidc_login_provider_url "https://auth.local/realms/vcc"
 setString oidc_login_client_id "nextcloud"
 setString oidc_login_client_secret "${OIDC_CLIENT_SECRET}"
 setBoolean oidc_login_end_session_redirect true
-setString oidc_login_logout_url "http://nextcloud/apps/oidc_login/oidc"
+setString oidc_login_logout_url "https://cloud.local/apps/oidc_login/oidc"
 setBoolean oidc_login_auto_redirect false
 setBoolean oidc_login_redir_fallback true
 
 runOCC config:system:set --value=preferred_username --type=string -- oidc_login_attributes id
 runOCC config:system:set --value=email --type=string -- oidc_login_attributes mail
+
+apache2-foreground
